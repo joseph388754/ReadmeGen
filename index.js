926c0e840b931d23733cf0a7d7194f1f2d1e6da2
unknown <joseph_malonzo100@yahoo.com>
2023-08-24 23:30:53 -0700

    files added, updated index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadMe = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {   
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub Username?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+
+    {   
+        type: 'input',
+        name: 'description',
+        message: 'Give a brief description of your project!'
+    },
+
+
+    {   
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license!',
+        choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause', 'Unlicense', 'BSD 2-clause', 'LGPLv3', 'AGPLv3', 'Other']
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+    },
+
+    {   
+        type: 'input',
+        name: 'test',
+        message: 'Explain testing procedures and instructions on how to run necessary tests!'
+    },
+
+  
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, generateReadMe(data), (error) =>
+    err ? console.error(error) : console.log('Success!')
+  );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile('README.md', answers);
+    });
+}
+
+// Function call to initialize app
+init();
