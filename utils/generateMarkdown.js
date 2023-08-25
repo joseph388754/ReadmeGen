d918754fa6e0ae8e0237e35e7320f7d28ce9554e
unknown <joseph_malonzo100@yahoo.com>
2023-08-25 06:53:52 -0700

    final commit, readme file completed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,56 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function licenseBadge(data) {
+  return `![GitHub license](https://img.shields.io/badge/license-${data.license}-orange.svg)`; 
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function licenseInfo() {    
+    return `[License Info](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository)`
+}
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
+  ${data.description}
+
+  # Table of Contents
+  - [DESCRIPTION](#description)
+  
+  - [USAGE](#usage)
+  
+  - [INSTALLATION](#installation)
+  
+  - [LICENSE](#license)
+  
+  - [CREDITS/CONTRIBUTING](#contributing)
+  
+  - [TESTING](#test)
+  
+  - [CONTACT/QUESTIONS](#contactGH)
+
+  ## Usage
+  ${data.usage}
+
+  ## Installation
+  ${data.installation}
+  
+  ## License
+  ${licenseBadge(data.license)}
+
+  ${licenseInfo()}
+  
+  ## Credits/contributors:
+  ${data.contributing}
+
+  ## Testing
+  ${data.test}
+
+  ## Questions/Concerns/or contact me!
+  GitHub Profile: [${data.contactGH}](https://www.github.com/${data.contactGH})
+ 
+  Email: ${data.email}
+
 
 `;
 }
